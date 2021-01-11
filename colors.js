9947679504b2956accc590863c01df1e5016b9b3
Younghwan <younghwankim624@gmail.com>
2021-01-11 20:37:16 +0900

    use jQuery

--- colors.js
+++ colors.js
@@ -1,17 +1,20 @@
 var Links = {
     setColor:function (color) {
-        var as = document.getElementsByTagName('a');
-        for(var i=0;i<as.length;i++) {
-            as[i].style.color=color;
-        }
+        // var as = document.getElementsByTagName('a');
+        // for(var i=0;i<as.length;i++) {
+        //     as[i].style.color=color;
+        // }
+        $('a').css('color', color);
     }
 }
 var Body = {
     setColor:function (color) {
-        document.querySelector('body').style.color = color;
+        //document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor:function (color) {
-        document.querySelector('body').style.backgroundColor = color;
+        //document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 function nightDayHandler(self) {
